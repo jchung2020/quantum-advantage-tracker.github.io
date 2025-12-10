f1b19aaf661adfeb7fef17ae211c461b82c85c6f
Abdón Rodríguez <abdon@ibm.com>
2025-12-10 16:58:10 -0500

    Setup filters placeholder

--- src/app/trackers/classically-verifiable-problems/SubmissionsTable.tsx
+++ src/app/trackers/classically-verifiable-problems/SubmissionsTable.tsx
@@ -20,95 +20,103 @@ export function SubmissionsTable(props: {
   const circuitInstances = flattenInstances(circuitModels);
 
   return (
-    <Table className="min-w-330 table-fixed">
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-26">
-            Date <ArrowDownIcon size={16} className="float-end mt-0.5" />
-          </TableHead>
-          <TableHead className="w-64 min-w-64">Name / Institutions</TableHead>
-          <TableHead className="w-36">Method</TableHead>
-          <TableHead className="w-36">Circuit</TableHead>
-          <TableHead className="w-18">Qubits</TableHead>
-          <TableHead className="w-18">Gates</TableHead>
-          <TableHead className="w-48">Value</TableHead>
-          <TableHead className="w-28">
-            Runtime
-            <br />
-            (seconds)
-          </TableHead>
-          <TableHead className="w-56">Compute resources</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {submissions.length === 0 ? (
-          <TableBodyEmpty />
-        ) : (
-          sortSubmissions(submissions).map((submission, index) => {
-            const circuitInstance = circuitInstances.find(
-              (instance) => instance.id === submission.circuit,
-            )!;
+    <div>
+      {/* Filters */}
 
-            return (
-              <TableRow key={`submission-cvp-${index}`}>
-                <TableCell>
-                  <time dateTime={submission.createdAt} title={submission.createdAt}>
-                    {formatDate(submission.createdAt)}
-                  </time>
-                </TableCell>
-                <TableCell className="wrap-break-word whitespace-normal">
-                  <a
-                    href={submission.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-link-foreground hover:underline"
-                  >
-                    {submission.name}
-                  </a>
+      <Table className="min-w-330 table-fixed">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-26">
+              Date <ArrowDownIcon size={16} className="float-end mt-0.5" />
+            </TableHead>
+            <TableHead className="w-64 min-w-64">Name / Institutions</TableHead>
+            <TableHead className="w-36">Method</TableHead>
+            <TableHead className="w-36">Circuit</TableHead>
+            <TableHead className="w-18">Qubits</TableHead>
+            <TableHead className="w-18">Gates</TableHead>
+            <TableHead className="w-48">Value</TableHead>
+            <TableHead className="w-28">
+              Runtime
+              <br />
+              (seconds)
+            </TableHead>
+            <TableHead className="w-56">Compute resources</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {submissions.length === 0 ? (
+            <TableBodyEmpty />
+          ) : (
+            sortSubmissions(submissions).map((submission, index) => {
+              const circuitInstance = circuitInstances.find(
+                (instance) => instance.id === submission.circuit,
+              )!;
 
-                  <div className="mt-2">
-                    <span className="font-semibold text-green-600">By:</span>{' '}
-                    <span>{submission.institutions}</span>
-                  </div>
-                </TableCell>
-                <TableCell className="whitespace-normal">{submission.method}</TableCell>
-                <TableCell className="wrap-break-word whitespace-normal">
-                  <a
-                    href={getCircuitInstanceUrl('classically-verifiable-problems', circuitInstance)}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-link-foreground hover:underline"
-                  >
-                    {circuitInstance.id}
-                  </a>
-                </TableCell>
-                <TableCell>{circuitInstance.qubits}</TableCell>
-                <TableCell>{circuitInstance.gates}</TableCell>
-                <TableCell>{submission.value}</TableCell>
-                <TableCell>
-                  <div>
-                    <span title="Quantum">Q</span>:{' '}
-                    <RuntimeSeconds value={submission.runtimeQuantum} />
-                  </div>
-                  <div>
-                    <span title="Classical">C</span>:{' '}
-                    <RuntimeSeconds value={submission.runtimeClassical} />
-                  </div>
-                </TableCell>
-                <TableCell className="whitespace-normal">
-                  <div>
-                    <span title="Quantum">Q</span>: {submission.computeResourcesQuantum || '-'}
-                  </div>
-                  <div>
-                    <span title="Classical">C</span>: {submission.computeResourcesClassical || '-'}
-                  </div>
-                </TableCell>
-              </TableRow>
-            );
-          })
-        )}
-      </TableBody>
-    </Table>
+              return (
+                <TableRow key={`submission-cvp-${index}`}>
+                  <TableCell>
+                    <time dateTime={submission.createdAt} title={submission.createdAt}>
+                      {formatDate(submission.createdAt)}
+                    </time>
+                  </TableCell>
+                  <TableCell className="wrap-break-word whitespace-normal">
+                    <a
+                      href={submission.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-link-foreground hover:underline"
+                    >
+                      {submission.name}
+                    </a>
+
+                    <div className="mt-2">
+                      <span className="font-semibold text-green-600">By:</span>{' '}
+                      <span>{submission.institutions}</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="whitespace-normal">{submission.method}</TableCell>
+                  <TableCell className="wrap-break-word whitespace-normal">
+                    <a
+                      href={getCircuitInstanceUrl(
+                        'classically-verifiable-problems',
+                        circuitInstance,
+                      )}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-link-foreground hover:underline"
+                    >
+                      {circuitInstance.id}
+                    </a>
+                  </TableCell>
+                  <TableCell>{circuitInstance.qubits}</TableCell>
+                  <TableCell>{circuitInstance.gates}</TableCell>
+                  <TableCell>{submission.value}</TableCell>
+                  <TableCell>
+                    <div>
+                      <span title="Quantum">Q</span>:{' '}
+                      <RuntimeSeconds value={submission.runtimeQuantum} />
+                    </div>
+                    <div>
+                      <span title="Classical">C</span>:{' '}
+                      <RuntimeSeconds value={submission.runtimeClassical} />
+                    </div>
+                  </TableCell>
+                  <TableCell className="whitespace-normal">
+                    <div>
+                      <span title="Quantum">Q</span>: {submission.computeResourcesQuantum || '-'}
+                    </div>
+                    <div>
+                      <span title="Classical">C</span>:{' '}
+                      {submission.computeResourcesClassical || '-'}
+                    </div>
+                  </TableCell>
+                </TableRow>
+              );
+            })
+          )}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
 
--- src/app/trackers/variational-problems/SubmissionsTable.tsx
+++ src/app/trackers/variational-problems/SubmissionsTable.tsx
@@ -20,104 +20,109 @@ export function SubmissionsTable(props: {
   const hamiltonianInstances = flattenInstances(hamiltonians);
 
   return (
-    <Table className="min-w-330 table-fixed">
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-26">
-            Date <ArrowDownIcon size={16} className="float-end mt-0.5" />
-          </TableHead>
-          <TableHead className="w-64 min-w-64">Name / Institutions</TableHead>
-          <TableHead className="w-36">Method</TableHead>
-          <TableHead className="w-36">Hamiltonian</TableHead>
-          <TableHead className="w-18">Qubits</TableHead>
-          <TableHead className="w-18">Gates</TableHead>
-          <TableHead className="w-48">
-            Energy (Eh)
-            <br />
-            [upper, lower bound]
-          </TableHead>
-          <TableHead className="w-28">
-            Runtime
-            <br />
-            (seconds)
-          </TableHead>
-          <TableHead className="w-56">Compute resources</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {submissions.length === 0 ? (
-          <TableBodyEmpty />
-        ) : (
-          sortSubmissions(submissions).map((submission, index) => {
-            const hamiltonianInstance = hamiltonianInstances.find(
-              (instance) => instance.id === submission.hamiltonian,
-            )!;
+    <div>
+      {/* Filters */}
 
-            return (
-              <TableRow key={`submission-vp-${index}`}>
-                <TableCell>
-                  <time dateTime={submission.createdAt} title={submission.createdAt}>
-                    {formatDate(submission.createdAt)}
-                  </time>
-                </TableCell>
-                <TableCell className="whitespace-normal">
-                  <a
-                    href={submission.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-link-foreground hover:underline"
-                  >
-                    {submission.name}
-                  </a>
+      <Table className="min-w-330 table-fixed">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-26">
+              Date <ArrowDownIcon size={16} className="float-end mt-0.5" />
+            </TableHead>
+            <TableHead className="w-64 min-w-64">Name / Institutions</TableHead>
+            <TableHead className="w-36">Method</TableHead>
+            <TableHead className="w-36">Hamiltonian</TableHead>
+            <TableHead className="w-18">Qubits</TableHead>
+            <TableHead className="w-18">Gates</TableHead>
+            <TableHead className="w-48">
+              Energy (Eh)
+              <br />
+              [upper, lower bound]
+            </TableHead>
+            <TableHead className="w-28">
+              Runtime
+              <br />
+              (seconds)
+            </TableHead>
+            <TableHead className="w-56">Compute resources</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {submissions.length === 0 ? (
+            <TableBodyEmpty />
+          ) : (
+            sortSubmissions(submissions).map((submission, index) => {
+              const hamiltonianInstance = hamiltonianInstances.find(
+                (instance) => instance.id === submission.hamiltonian,
+              )!;
 
-                  <div className="mt-2">
-                    <span className="font-semibold text-green-600">By:</span>{' '}
-                    <span>{submission.institutions}</span>
-                  </div>
-                </TableCell>
-                <TableCell className="whitespace-normal">{submission.method}</TableCell>
-                <TableCell className="wrap-break-word whitespace-normal">
-                  <a
-                    href={getHamiltonianUrl(hamiltonianInstance)}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-link-foreground hover:underline"
-                  >
-                    {submission.hamiltonian}
-                  </a>
-                </TableCell>
-                <TableCell>{submission.qubits}</TableCell>
-                <TableCell>{submission.gates}</TableCell>
-                <TableCell className="wrap-break-word whitespace-normal">
-                  <div>{submission.energy}</div>
-                  <div>
-                    [{submission.errorBoundHigh || 'N/A'}, {submission.errorBoundLow || 'N/A'}]
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <div>
-                    <span title="Quantum">Q</span>:{' '}
-                    <RuntimeSeconds value={submission.runtimeQuantum} />
-                  </div>
-                  <div>
-                    <span title="Classical">C</span>:{' '}
-                    <RuntimeSeconds value={submission.runtimeClassical} />
-                  </div>
-                </TableCell>
-                <TableCell className="whitespace-normal">
-                  <div>
-                    <span title="Quantum">Q</span>: {submission.computeResourcesQuantum || '-'}
-                  </div>
-                  <div>
-                    <span title="Classical">C</span>: {submission.computeResourcesClassical || '-'}
-                  </div>
-                </TableCell>
-              </TableRow>
-            );
-          })
-        )}
-      </TableBody>
-    </Table>
+              return (
+                <TableRow key={`submission-vp-${index}`}>
+                  <TableCell>
+                    <time dateTime={submission.createdAt} title={submission.createdAt}>
+                      {formatDate(submission.createdAt)}
+                    </time>
+                  </TableCell>
+                  <TableCell className="whitespace-normal">
+                    <a
+                      href={submission.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-link-foreground hover:underline"
+                    >
+                      {submission.name}
+                    </a>
+
+                    <div className="mt-2">
+                      <span className="font-semibold text-green-600">By:</span>{' '}
+                      <span>{submission.institutions}</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="whitespace-normal">{submission.method}</TableCell>
+                  <TableCell className="wrap-break-word whitespace-normal">
+                    <a
+                      href={getHamiltonianUrl(hamiltonianInstance)}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-link-foreground hover:underline"
+                    >
+                      {submission.hamiltonian}
+                    </a>
+                  </TableCell>
+                  <TableCell>{submission.qubits}</TableCell>
+                  <TableCell>{submission.gates}</TableCell>
+                  <TableCell className="wrap-break-word whitespace-normal">
+                    <div>{submission.energy}</div>
+                    <div>
+                      [{submission.errorBoundHigh || 'N/A'}, {submission.errorBoundLow || 'N/A'}]
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div>
+                      <span title="Quantum">Q</span>:{' '}
+                      <RuntimeSeconds value={submission.runtimeQuantum} />
+                    </div>
+                    <div>
+                      <span title="Classical">C</span>:{' '}
+                      <RuntimeSeconds value={submission.runtimeClassical} />
+                    </div>
+                  </TableCell>
+                  <TableCell className="whitespace-normal">
+                    <div>
+                      <span title="Quantum">Q</span>: {submission.computeResourcesQuantum || '-'}
+                    </div>
+                    <div>
+                      <span title="Classical">C</span>:{' '}
+                      {submission.computeResourcesClassical || '-'}
+                    </div>
+                  </TableCell>
+                </TableRow>
+              );
+            })
+          )}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
 
