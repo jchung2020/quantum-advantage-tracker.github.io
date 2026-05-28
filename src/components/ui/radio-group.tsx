fd1fe7b43118623c30c424f71f21884e3a37b48d
Abdón Rodríguez <abdon@ibm.com>
2026-05-28 11:14:08 -0400

    Update radio button with a check

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { CircleIcon } from "lucide-react"
+import { CheckIcon } from "lucide-react"
 import { RadioGroup as RadioGroupPrimitive } from "radix-ui"
 
 import { cn } from "@/lib/utils"
@@ -27,7 +27,7 @@ function RadioGroupItem({
     <RadioGroupPrimitive.Item
       data-slot="radio-group-item"
       className={cn(
-        "aspect-square size-4 shrink-0 rounded-full border border-input text-foreground shadow-xs transition-[color,box-shadow] outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:bg-input/30 dark:aria-invalid:ring-destructive/40",
+        "aspect-square size-4 shrink-0 rounded-full border border-input text-foreground shadow-xs transition-[color,box-shadow,background-color] outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 data-[state=checked]:border-foreground data-[state=checked]:bg-foreground data-[state=checked]:text-background dark:bg-input/30 dark:aria-invalid:ring-destructive/40",
         className
       )}
       {...props}
@@ -36,7 +36,7 @@ function RadioGroupItem({
         data-slot="radio-group-indicator"
         className="relative flex items-center justify-center"
       >
-        <CircleIcon className="absolute top-1/2 left-1/2 size-2 -translate-x-1/2 -translate-y-1/2 fill-foreground" />
+        <CheckIcon className="absolute top-1/2 left-1/2 size-3 -translate-x-1/2 -translate-y-1/2 stroke-3" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   )
